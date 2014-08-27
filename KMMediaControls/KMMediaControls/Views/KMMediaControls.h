99d69cc8b67e07b8c1d088d80e7ea2534fc78774
Kieran McGrady <kieran@hotrodsoftware.com>
2014-08-27 22:28:36 +0100

    Initial commit

---
+++ KMMediaControls/KMMediaControls/Views/KMMediaControls.h
@@ -0,0 +1,13 @@
+//
+//  KMMediaControls.h
+//  KMMediaControls
+//
+//  Created by Kieran McGrady on 27/08/2014.
+//  Copyright (c) 2014 Kieran McGrady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMMediaControls : UIView
+
+@end
---
+++ KMMediaControls/KMMediaControls/Views/KMMediaControls.m
@@ -0,0 +1,13 @@
+//
+//  KMMediaControls.m
+//  KMMediaControls
+//
+//  Created by Kieran McGrady on 27/08/2014.
+//  Copyright (c) 2014 Kieran McGrady. All rights reserved.
+//
+
+#import "KMMediaControls.h"
+
+@implementation KMMediaControls
+
+@end
