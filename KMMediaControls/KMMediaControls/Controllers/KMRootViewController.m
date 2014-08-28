0337ed30157ad114de45bebff180cfbc1300f8ed
Kieran McGrady <kieran@hotrodsoftware.com>
2014-08-28 12:37:06 +0100

    Set background color

--- KMMediaControls/KMMediaControls/Controllers/KMRootViewController.m
+++ KMMediaControls/KMMediaControls/Controllers/KMRootViewController.m
@@ -17,6 +17,8 @@
 
 - (void)viewDidLoad
 {
+    self.view.backgroundColor = [UIColor colorWithRed:75/255.0f green:201/255.0f blue:166/255.0f alpha:1.0f];
+    
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"audio" ofType:@"mp3"];
     NSURL *fileURL = [NSURL fileURLWithPath:filePath];
     KMMediaControls *mediaControls = [[KMMediaControls alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height-120, self.view.frame.size.width, 120)
