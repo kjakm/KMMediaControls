48df4fed3d952f652d0c6110e3692d4e40571005
Kieran McGrady <kieran@hotrodsoftware.com>
2014-08-28 13:29:51 +0100

    Added documentation for enum

--- KMMediaControls.h
+++ KMMediaControls.h
@@ -9,6 +9,13 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 
+/**
+ This enum is used to set the behaviour of the stop button. The default mode (i.e. if the StopAudio variable
+ isn't set) is to use StopAudioPause. StopAudioPause pauses playback when stop is pressed and resumes from the
+ same position when play is pressed. StopAudioReset stops the audio when stop is pressed and resets the playback
+ position to 0.0seconds. Playback would resume from the start.
+ */
+
 typedef enum : NSUInteger {
     StopAudioReset,
     StopAudioPause,
